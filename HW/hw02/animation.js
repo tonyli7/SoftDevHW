3d9452e7a58ea881de68e013981f7c9984dd13af
tonyli7 <xxtonyxxlixx@gmail.com>
2016-02-23 09:42:38 -0500

    stop

--- HW/hw02/animation.js
+++ HW/hw02/animation.js
@@ -1,5 +1,5 @@
-var b = document.getElementById("circle");//clear button object
-
+var b = document.getElementById("circle");//circle button object
+var s = document.getElementById("stop");//stop button object
 var c = document.getElementById("playground");//canvas object
 var ctx = c.getContext("2d");
 
@@ -10,7 +10,9 @@ ctx.strokeRect(0,0,538,538);
 //-------------------------
 var radius = 0;
 var growing = true;
+
 function draw_circle(e){
+   
     if (growing){
 	radius++;
     }else{
@@ -29,7 +31,14 @@ function draw_circle(e){
     ctx.stroke();
     ctx.fill();
 
-    window.requestAnimationFrame(draw_circle);
+    requestID=window.requestAnimationFrame(draw_circle)
+    
+}
+
+function stop(e){
+  
+    window.cancelAnimationFrame(requestID);
 }
 
 b.addEventListener("click",draw_circle);
+s.addEventListener("click", stop);
