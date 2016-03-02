898551270a136d735ba3dc60f77c51a7cba43159
tonyli7 <xxtonyxxlixx@gmail.com>
2016-03-02 10:25:13 -0500

    circle animation

--- svg.js
+++ svg.js
@@ -1,4 +1,7 @@
 var pic = document.getElementById("vimage");
+var circle = document.getElementById("circle");
+var stop = document.getElementById("stop");
+
 console.log(pic);
 var change = function(e) {
     e.preventDefault();
@@ -22,14 +25,44 @@ var clicked = function(e){
 	drawDot(e.offsetX,e.offsetY);
     }
 };
-
-var grow = true;
+//================Circle Animate=======================
+var grow = 1;
 var dr = 1;
+var radius = 0;
 var circle_animate = function(e){
-    radius = parseInt(c.getAttribute("r"));
+     var c = document.createElementNS(
+	"http://www.w3.org/2000/svg","circle");
+    //setup
+    c.setAttribute("cx",250);
+    c.setAttribute("cy",250);
+    c.setAttribute("fill", "yellow");
+    c.setAttribute("stroke","black");
+    
+    radius+=dr*grow//grow if grow>0 ; shrink if grow<0
+    console.log(radius);
     c.setAttribute("r", radius.toString())
+    console.log(radius);
+    if (radius>=250){//if max size 
+	grow=-1
+    }else if (radius <=1){//if min size
+	grow=1
+    }
+    pic.appendChild(c);
+    pic.removeChild(pic.childNodes[0]);//removes last child
+  
 }
 
+var go_circle = function(e){
+    intervalID=window.setInterval(circle_animate,1);
+}
+
+var stop_circle = function(e){
+    window.clearInterval(intervalID);
+}
+//======================================================
+
+circle.addEventListener("click", go_circle);
+stop.addEventListener("click", stop_circle);
 pic.addEventListener("click",clicked);
 
 
