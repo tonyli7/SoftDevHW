8382ba8b8050ca3f014246dc81ea5e7a96339bea
tonyli7 <xxtonyxxlixx@gmail.com>
2016-04-01 10:02:26 -0400

    hello

--- HW/hw06/lab_listcomp.py
+++ HW/hw06/lab_listcomp.py
@@ -1,3 +1,4 @@
+import math
 """
 YOUR TASK The First:
 Write a function that uses list comprehension to return whether a password meets a minimum threshold: it contains a mixture of upper- and lowercase letters, and at least one number.
@@ -19,5 +20,33 @@ LC_LETTERS="abcdefghijklmnopqrstuvwxyz"
 NUMS="0123456789"
 SPECIAL=".?!&#,;:-_*"
 
+def isMin(pwd):
+    check=[1 if x in LC_LETTERS
+           else 2 if x in UC_LETTERS
+           else 3 if x in NUMS
+           else 4
+           for x in pwd]
+    if 1 or 2 or 3 in pwd:
+        return check
+    return False
 
-print [1 if x in LC_LETTERS]
+def ratePass(pwd):
+    rating=1
+    check=isMin(pwd) 
+    if isMin(pwd):
+        if len(check)>=8:
+            rating+=(len(check)*5)**(.5)
+        if 4 in check:
+            rating+=check.count(4)*2
+        return rating
+            
+    return """
+must have:
+lower-case\n
+upper-case\n
+at least one number\n
+"""
+        
+    
+print ratePass(p)
+print ratePass("hellohello")
---
+++ HW/hw07/lab_closures.py
@@ -0,0 +1,9 @@
+"""
+TASK: 
+Write a closure with outer function named 'repeat'
+ such that the function calls shown below will yield the outputs shown.
+
+print r1(2) -> hellohello
+print r2(2) -> goodbyegoodbye
+print repeat('cool')(3) -> coolcoolcool
+"""
