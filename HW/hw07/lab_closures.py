5b60b6db6214a3edfd57cba30b7298fa6380786d
tonyli7 <xxtonyxxlixx@gmail.com>
2016-04-01 10:45:04 -0400

    I think im done

--- HW/hw07/lab_closures.py
+++ HW/hw07/lab_closures.py
@@ -7,3 +7,11 @@ print r1(2) -> hellohello
 print r2(2) -> goodbyegoodbye
 print repeat('cool')(3) -> coolcoolcool
 """
+
+def repeat(x):
+    return lambda y: x*y
+
+r1=repeat('hello')
+r2=repeat('goodbye')
+print r1(2)
+print r2(2)
